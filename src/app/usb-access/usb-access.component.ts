9eac94cb4a4cafa19a1330a5732d904b0769a51a
priyankathakare87 <55233003+priyankathakare87@users.noreply.github.com>
2020-04-22 16:07:44 +0530

    validation reactive form concepts 22.04.2020

--- src/app/usb-access/usb-access.component.ts
+++ src/app/usb-access/usb-access.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-usb-access',
@@ -7,20 +7,30 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./usb-access.component.css']
 })
 export class UsbAccessComponent implements OnInit {
-  accessList: string[] = ['USB', 'DVD'];
 
-  usbaccessform: FormGroup = new FormGroup({
-    accessfor: new FormControl(),
-    date: new FormControl(new Date()),
-    reason: new FormControl(),
+  usbaccessForm = new FormGroup({
+    accessfor: new FormControl('', Validators.required),
+    datepick: new FormControl('', Validators.required),
+    reason: new FormControl('', Validators.required),
   });
+
+  accessList: string[] = ['USB', 'DVD'];
 
   constructor() { }
 
   ngOnInit() {
   }
+
+  /* Conver date object to string */
+  date(e) {
+    var convertDate = new Date(e.target.value).toISOString().substring(0, 10);
+    this.usbaccessForm.get('datepick').setValue(convertDate, {
+      onlyself: true
+    })
+  }
+
   onSubmit(){
     // TODO: Use EventEmitter with form value
-    console.warn(this.usbaccessform.value);
+    console.warn(this.usbaccessForm.value);
   }
 }
--- src/app/webmail-access/webmail-access.component.ts
+++ src/app/webmail-access/webmail-access.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { WebmailRequest } from '../model/webmail-request';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-webmail-access',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WebmailAccessComponent implements OnInit {
 
+  webmailaccessForm = new FormGroup({
+    currentEmailID: new FormControl('', [Validators.required, Validators.email]),
+  });
+  
+  formsubmitted = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public webmailAccessReq: WebmailRequest = {
+    reqNo : 'WA1',
+    reqSrNo: 12,
+    processId: 'W32',
+    reqBy: '4679',
+    emailId: 'priyanka.thakare@sci.co.in',
+    reqDate: null,
+    reqTime: null,
+    workflowId: 'abc123'
+  };
+  
+  get f(){ return this.webmailaccessForm.controls; }
+
+  onSubmit() {
+    this.formsubmitted = true;
+    if(this.webmailaccessForm.invalid){
+      return;
+    }
+    else{
+      // TODO: Use EventEmitter with form value
+    console.warn(this.webmailaccessForm.value);
+    alert('Success!')
+    }
+    
+  }
 }
